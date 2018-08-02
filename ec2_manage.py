e951fc97943ea34b2f014fe2548f9d49ebccfec8
obrutjack <obrutjack@yahoo.com>
2018-08-02 17:17:16 +0800

    List volumes and snapshots. Naive implementation of snapshot command

--- ec2_manage.py
+++ ec2_manage.py
@@ -14,11 +14,79 @@ def filter_instances(project):
         instances = ec2.instances.all()
 
     return instances
-   
 
 @click.group()
+def cli():
+    """Commands for manages snapshots"""
+
+@cli.group('snapshots')
+def snapshots():
+    """Commands for snapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None,
+    help="Only snapshot for project (tag Project:<name>)")
+def list_volumes(project):
+    "List EC2 snapshots"
+    
+    instances = filter_instances(project)
+   
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(", ".join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime("%c")
+                )))
+    return
+
+
+@cli.group('volumes')
+def volumes():
+    """Commands for volumes"""
+
+@volumes.command('list')
+@click.option('--project', default=None,
+    help="Only volume for project (tag Project:<name>)")
+def list_volumes(project):
+    "List EC2 volumes"
+
+    instances = filter_instances(project)
+   
+    for i in instances:
+        for v in i.volumes.all():
+            print(", ".join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + "GiB",
+                v.encrypted and "Encrypted" or "Not Encrypted"
+            )))
+    return
+
+@cli.group('instances')
 def instances():
-    """Commands for instances management"""
+    """Commands for instances"""
+
+@instances.command('snapshot',
+    help="Create snapshots of all volumes")
+@click.option('--project', default=None,
+    help="Only instance for project (tag Project:<name>)")
+def create_snapshots(project):
+    "Create snapshots for EC2 instances"
+
+    instances = filter_instances(project)
+    
+    for i in instances:
+        i.stop()
+        for v in i.volumes.all():
+            print("Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by ec2_manage.py")
+    return
 
 @instances.command('list')
 @click.option('--project', default=None,
@@ -77,5 +145,5 @@ def start_instances(project):
     return
 
 if __name__ == '__main__':
-    instances()
+    cli()
 
