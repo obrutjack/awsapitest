7c2c93aa7924e9d837b602231dbf87b7bd351507
obrutjack <obrutjack@yahoo.com>
2018-08-02 14:30:07 +0800

    Update script to use the click module, and show more information about instance

--- ec2_list.py
+++ ec2_list.py
@@ -1,8 +1,23 @@
 import boto3
+import click
+    
+session = boto3.Session(profile_name='awsps')
+ec2 = session.resource('ec2','us-west-2')
+
+@click.command()
+def list_instances():
+    "List EC2 instances"
+    for i in ec2.instances.all():
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            #i.key_name,
+            i.private_ip_address,
+            i.public_dns_name)))
+    return
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='awsps')
-    ec2 = session.resource('ec2','us-west-2')
+    list_instances()
 
-    for i in ec2.instances.all():
-        print(i)
